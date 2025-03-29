6eb0dbb41cc688e01b0c4068ad50b1339200a008
CarlosEduardo113 <ce219560@gmail.com>
2025-03-29 20:28:22 -0300

    Finalizar

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿   
+         // NomeCompleto - Exercício 1 da Lista 2
+
+        Console.Write("Por favor, digite seu primeiro nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Agora digite seu sobrenome: ");
+        string sobrenome = Console.ReadLine();
+
+        Console.WriteLine($"\nNome completo: {nome} {sobrenome}");
+        Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
